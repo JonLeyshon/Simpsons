a32bd4232cc28a0a6cda949deb540c683b3f150b
Jon Leyshon <leyshonjon@gmail.com>
2024-02-02 20:47:58 +0000

    Search bar, filter not working correctly

--- simpsons-quote-app/src/ components/UserSearch.jsx
+++ simpsons-quote-app/src/ components/UserSearch.jsx
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 class UserSearch extends Component {
   state = {};
   render() {
+    const { data, searchTerm, handleSearchTerm } = this.props;
+    console.log(searchTerm);
     return (
       <>
-        <input type="text" placeholder="Search for a Character" />
+        <input
+          type="text"
+          onInput={(e) => {
+            this.setState({ searchTerm: e.target.value });
+            handleSearchTerm(this.state.searchTerm);
+          }}
+        />
       </>
     );
   }
--- simpsons-quote-app/src/App.js
+++ simpsons-quote-app/src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from "react";
-import "./App.css";
 import axios from "axios";
-import UserInterface from "./ components/UserInterface";
-
+import UserSearch from "./ components/UserSearch";
+import "./App.css";
 class App extends Component {
   state = {};
+
+  onSearchInput = (e) => {
+    this.setState({ searchInput: e.target.value });
+    this.handleFilteredData();
+  };
+
   async componentDidMount() {
     const { data } = await axios.get(
       `https://thesimpsonsquoteapi.glitch.me/quotes?count=100`
     );
     this.setState({ quotes: data });
   }
+
+  handleFilteredData = () => {
+    const quotesCopy = [...this.state.quotes];
+    const filteredArray = quotesCopy.filter((quote) => {
+      return quote.character.includes(this.state.searchInput);
+    });
+
+    //ChatGPT code:
+    if (filteredArray.length > 0) {
+      this.setState({ quotes: filteredArray });
+    } else {
+      // If the filtered array is empty, you may want to handle it accordingly
+      // For example, reset the quotes to the original array
+      this.setState({ quotes: quotesCopy });
+    }
+
+    // this.setState({ quotes: filteredArray });
+  };
+
   render() {
     console.log(this.state);
-    if (!this.state.quotes) {
-      return <p>Loading...</p>;
-    }
     return (
       <>
-        <UserInterface data={this.state} />
+        <input
+          type="text"
+          placeholder="Search your Character"
+          onInput={this.onSearchInput}
+        ></input>
       </>
     );
   }
